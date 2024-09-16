57a9df0fc8f5073391144e8ed8b42f4dc3d66a6c
mtaltunkanat <muhammedaltunkanat.kariyer@gmail.com>
2024-09-16 17:02:05 +0300

    oop activity

---
+++ js/0-Başlangıç/öğreti.js
@@ -0,0 +1,9 @@
+let araba = {
+    renk:"mavi",
+    hiz:0,
+};
+//mor metod
+//mavi property komutlarda
+console.log(araba.hiz);
+console.log(araba.hiz);//örnekler bunlar
+// mesela arabaya bakıcaz ama araba. yazıp sadece istediğimiz parametrenin gelmesini sağlayabiliriz
